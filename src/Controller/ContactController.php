0d9e5c04bf1f4a99afb63fa848edfee4bf5877dd
ArthurBrunet <Arthur.brunet76@gmail.com>
2021-04-02 16:57:10 +0200

    crud Contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,81 +2,107 @@
 
 namespace App\Controller;
 
+use App\Entity\Company;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use App\Repository\ContactRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/contact')]
 class ContactController extends AbstractController
 {
-    #[Route('/', name: 'contact_index', methods: ['GET'])]
-    public function index(ContactRepository $contactRepository): Response
+    /**
+     * @Route("/get", name="User", methods={"GET"})
+     * @param ContactRepository $contactRepository
+     * @param SerializerInterface $serializer
+     * @param Request $request
+     * @return JsonResponse
+     *
+     * Gére le get avec un ou plusieur champs (tout les champs possible dans l'entité) et le getall si rien n'est renseigné
+     */
+    public function find(ContactRepository $contactRepository, SerializerInterface $serializer,Request $request)
     {
-        return $this->render('contact/index.html.twig', [
-            'contacts' => $contactRepository->findAll(),
-        ]);
+        $filter = [];
+        $em = $this->getDoctrine()->getManager();
+        $metadata = $em->getClassMetadata(ContactRepository::class)->getFieldNames();
+        $content = (array) json_decode($request->getContent());
+        foreach($metadata as $value){
+            if (isset($content[$value])){
+                $filter[$value] = $content[$value];
+            }
+        }
+        return JsonResponse::fromJsonString($serializer->serialize($contactRepository->findBy($filter),"json"),Response::HTTP_OK);
     }
 
-    #[Route('/new', name: 'contact_new', methods: ['GET', 'POST'])]
-    public function new(Request $request): Response
+    #[Route('/{id}/new', name: 'contact_new', methods: ['POST'])]
+    public function new(Company $company = null, Request $request,ValidatorInterface $validator ,EntityManagerInterface $em): Response
     {
+        if (!$company) {
+            return new JsonResponse("Company not found", Response::HTTP_BAD_REQUEST);
+        }
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
-        $form->handleRequest($request);
+        return $this->validAndInsert($request, $form, $validator, $contact, $em) ?
+            new JsonResponse("Contact created", Response::HTTP_OK) :
+            new JsonResponse("Bad request", Response::HTTP_BAD_REQUEST);
+    }
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($contact);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('contact_index');
+    #[Route('/{id}/edit', name: 'contact_edit', methods: ['POST'])]
+    public function edit(Request $request, Contact $contact = null, EntityManagerInterface $em, ValidatorInterface $validator): Response
+    {
+        if (!$contact) {
+            return new JsonResponse("Contact not found", Response::HTTP_BAD_REQUEST);
         }
 
-        return $this->render('contact/new.html.twig', [
-            'contact' => $contact,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    #[Route('/{id}', name: 'contact_show', methods: ['GET'])]
-    public function show(Contact $contact): Response
-    {
-        return $this->render('contact/show.html.twig', [
-            'contact' => $contact,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'contact_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Contact $contact): Response
-    {
         $form = $this->createForm(ContactType::class, $contact);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('contact_index');
-        }
-
-        return $this->render('contact/edit.html.twig', [
-            'contact' => $contact,
-            'form' => $form->createView(),
-        ]);
+        return $this->validAndInsert($request, $form, $validator, $contact, $em) ?
+            new JsonResponse("Contact updated", Response::HTTP_OK) :
+            new JsonResponse("Bad request", Response::HTTP_BAD_REQUEST);
     }
 
     #[Route('/{id}', name: 'contact_delete', methods: ['POST'])]
-    public function delete(Request $request, Contact $contact): Response
+    public function delete(Request $request, Contact $contact = null): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$contact->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($contact);
-            $entityManager->flush();
+        if (!$contact) {
+            return new JsonResponse("Contact not found", Response::HTTP_BAD_REQUEST);
         }
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($contact);
+        $entityManager->flush();
+        return new JsonResponse('Contact deleted', Response::HTTP_OK);
+    }
 
-        return $this->redirectToRoute('contact_index');
+    /**
+     * @param Request $request
+     * @param FormInterface $form
+     * @param ValidatorInterface $validator
+     * @param Contact $contact
+     * @param EntityManagerInterface $em
+     * @return boolean
+     */
+    public function validAndInsert(Request $request, FormInterface $form, ValidatorInterface $validator, Contact $contact, EntityManagerInterface $em): bool
+    {
+        $json_decode = json_decode($request->getContent(), true);
+        $form->submit($json_decode);
+        $validate = $validator->validate($contact, null, 'Register');
+        if (count($validate) !== 0) {
+            foreach ($validate as $error) {
+                return false;
+            }
+        }
+        $em->persist($contact);
+        $em->flush();
+
+        return true;
     }
 }
